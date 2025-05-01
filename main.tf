68a3bfd4d04a818aaa5d59682fb43660a1d3852b
rush1998 <rushabh.m.patel1998@gmail.com>
2025-04-30 22:48:37 -0400

    dev1

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3-terraform-state-45455"
+    key            = "global/s3/terraform.tfstate"  # Or per env: dev/network/terraform.tfstate
+    region         = "us-east-1"
+    dynamodb_table = "devops-course-DB"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = "10.0.0.0/16"
+  name       = "main-vpc"
+}
